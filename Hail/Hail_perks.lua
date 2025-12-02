340e335dc30c90f3e56b06fbd9046d26a501418f
EHaakma <emiel.haakma@gmail.com>
2025-12-03 00:14:43 +0100

    Hail added

---
+++ Hail/Hail_perks.lua
@@ -0,0 +1,71 @@
+-- Perk List
+perks = {
+  [1]={
+    remove={"(-2)"},
+    add={"(+0)"},
+  },
+  [2]={
+    remove={"(-1)"},
+    add={"(+0) wound or immobilize"},
+  },
+  [3]={
+    remove={"(-1)"},
+    add={"(+0) wound or immobilize"},
+  },
+  [4]={
+    remove={"(-1)"},
+    add={"(+1) wild"},
+  },
+  [5]={
+    remove={"(-1)"},
+    add={"(+1) wild"},
+  },
+  [6]={
+    remove={"(+0)"},
+    add={"(+1) poison or muddle"},
+  },
+  [7]={
+    remove={"(+0)"},
+    add={"(+1) poison or muddle"},
+  },
+  [8]={
+    remove={"(+0)"},
+    add={"(+1) ((heal) 1, (target) 1 ally) rolling"},
+  },
+  [9]={
+    remove={"(+0)"},
+    add={"(+1) ((heal) 1, (target) 1 ally) rolling"},
+  },
+  [10]={
+    remove={"(+1)","(+1)"},
+    add={"(+2)","(+2)"},
+  },
+  [11]={
+    add={"(+1) stun"},
+  },
+  [12]={
+    add={"(+1) stun"},
+  },
+  [13]={
+    add={"(+3) (safeguard, (target) 1 ally)"},
+  },
+  [14]={
+    add={"(+3) (safeguard, (target) 1 ally)"},
+  },
+  [15]={
+    ignore=ClassApi.PerkType.IgnoreScenario,
+    add={"(+1)","(+1)"},
+  },
+  [16]={
+    --Whenever you short rest, you may choose which card to lose
+    add={"Perk Reminder #16"},
+  },
+  [17]={
+    --Your and your allies' enhancements cost five gold less and whenever an ally enhances a card, you gain five gold
+    add={"Perk Reminder #17"},
+  },
+  [18]={
+    --Add one (+1) ((icon2) gains 1 (icon)) card to the monster attack modifier deck
+    add={"Perk Reminder #18"},
+  },
+},
